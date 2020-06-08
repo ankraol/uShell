a7854130d9a7d173876552e91a2bc548aeff3dfc
Oleksandr Stanislavskyi <ostanislav@e1r6p5.unit.ua>
2020-06-08 22:00:18 +0300

    error case

--- src/mx_cd_command.c
+++ src/mx_cd_command.c
@@ -6,8 +6,10 @@ void mx_struct_flag_cd(char *av, t_builtin_command *command, char *flag, int *er
 			command->cd->flag_s = true;
 		else if (av[j] == flag[1])
 			command->cd->flag_P = true;
-		else if (av[j] > 47 && av[j] < 58)
+		else if (av[j] > 47 && av[j] < 58) {
 			fprintf(stderr, "cd: no such entry in dir stack\n");
+            *err = 1;
+        }
 		else {
 			fprintf(stderr, "cd: no such file or directory: -%c\n", av[j]);
 			*err = 1;
---
+++ src/mx_mistake.c
@@ -0,0 +1,26 @@
+#include "header.h"
+
+void mx_del_all(char ***argv, char **path) {
+    if (*argv != NULL)
+        mx_del_strarr(argv);
+    if (*path != NULL)
+        mx_strdel(path);
+}
+
+
+
+
+void mx_mistake(char *command, char ***argv, char **path, bool flag) {
+    if (flag) {
+        mx_printerr("ush: ");
+        mx_printerr("command not found: ");
+        mx_printerr(command);
+        mx_printerr("\n");
+    }
+    else{
+        mx_printerr("env: ");
+        mx_printerr(command);
+        mx_printerr(": No such file or directory\n");
+    }
+    mx_del_all(argv, path);
+}
